4f7637d1bdea6ba93b4f6f87acb161db0d0ab64b
Gustavo Fernando <gu.bressan@outlook.com>
2022-07-02 00:06:09 -0300

    filtro emplementado

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,12 +7,7 @@ function Home() {
   const [ data, setData ] = useState([]);
   const [ itensPerPage] = useState(9);
   const [ currentPage, setCurrentPage ] = useState(0);
-
-  //Paginação.
-  const pages = Math.ceil(data.length / itensPerPage)
-  const startIndex = currentPage * itensPerPage;
-  const endIndex = startIndex + itensPerPage;
-  const currentItens = data.slice(startIndex, endIndex);
+  const [ filterItems, setFilterItems] = useState(500);
 
   useEffect(() => {
     fetch('https://wine-back-test.herokuapp.com/products')
@@ -20,25 +15,88 @@ function Home() {
     .then((json) => setData(json.items));
   },[])
 
+  //Paginação.
+  const pages = Math.ceil(data.length / itensPerPage)
+  const startIndex = currentPage * itensPerPage;
+  const endIndex = startIndex + itensPerPage;
+  const currentItens = data.slice(startIndex, endIndex);
+
+
+  //Filtro
+  const handleChange = (event) => {
+    const filtered = data.filter(itens => {
+      return itens.price <= event.target.value;
+     
+    });
+    setData(filtered);
+    console.log(filtered)
+  }
+
 
   return (
     <div className='home'>
       <Header />
-      <p>{`${data.length} produtos encontrados`}</p>
+      <div className='mainCont'>
+      <p className='found'>{`${data.length} produtos encontrados`}</p>
+      <div>
+      <h1>Refine sua busca</h1>
+      <p>Por preço</p>
 
-      <div className='cont'>
+      <input
+        type="radio"
+        value="40"
+        onChange={handleChange}
+
+      />Até R$40<br/>
+
+        <input
+        type="radio"
+        value="60"
+        onChange={handleChange}
+      />R$40 A R$60<br/>
+        
+        <input
+        type="radio"
+        value="100"
+
+        onChange={handleChange}
+      />R$100 A R$200<br/>
+        
+        <input
+        type="radio"
+        value="200"
+        onChange={handleChange}
+      />R$200 A R$500<br/>
+       
+       <input
+        type="radio"
+        value="500"
+      
+        onChange={handleChange}
+      />Acima de R$500<br/>
+
+      </div>
+
+      <div className='conteudo'>
+        
         {currentItens.map((item) =>
        <div className='cardItem' key={item.id}>
-        {item.name}
-        <img src={item.image} />
-        {item.price}
+        
+        <img src={item.image} className='wine'/>
+        <p>{item.name}</p>
+       {/* colocar desconto */}
+        
+        <p>{`Sócio WINE R$${item.priceMember}`}</p>
+        <p>{`NÃO SOCIO R$${item.price}`}</p>
         <button>Adicionar</button>
        </div> )}
-      </div>
-      <div>
+        </div>
+        </div>
+        <div>
           {Array.from(Array(pages),(data,index) => {
             return <button value={index} onClick={(e) => setCurrentPage(Number(e.target.value))}>{index}</button>
           })}
+
         </div>
     </div>
   );
