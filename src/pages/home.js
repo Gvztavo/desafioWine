1fa659edef13f2ce37c6a05749b261944797317e
Gustavo Fernando <gu.bressan@outlook.com>
2022-07-03 21:21:23 -0300

    ajustes css

--- src/pages/home.js
+++ src/pages/home.js
@@ -59,8 +59,9 @@ function Home() {
   return (
     <div className='home'>
       <Header />
-      <div className='mainCont'>
       <p className='found'>{`${data.length} produtos encontrados`}</p>
+      <div className='mainCont'>
+      
       <div>
       <h1>Refine sua busca</h1>
       <p>Por preço</p>
@@ -123,7 +124,7 @@ function Home() {
        </div> )}
         </div>
         </div>
-        <div>
+        <div className='pag'>
           {Array.from(Array(pages),(data,index) => {
             return <button value={index} onClick={(e) => setCurrentPage(Number(e.target.value))}>{index}</button>
           })}
